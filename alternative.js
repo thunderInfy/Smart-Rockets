96854b0d539d241d54a7fe45774a845b8f0ce76c
thunderInfy <r.aditya0824@gmail.com>
2019-05-30 00:20:50 -0700

    genetic algorithm implemented

---
+++ alternative.js
@@ -0,0 +1,29 @@
+//alternative
+// let completedIndices = [];
+
+// for(let i=0; i<this.members.length; i++){
+// 	if(this.members[i].completed){
+// 		completedIndices.push(i);
+// 	}
+// }
+
+// completedIndices.sort((a,b) => this.members[a].completedLifeTime - this.members[b].completedLifeTime);
+
+// for(let i=0; i<completedIndices.length; i++){
+// 	if(i!=0 && this.members[completedIndices[i]].completedLifeTime!= this.members[completedIndices[i-1]].completedLifeTime){
+// 		maxFit++;
+// 	}
+
+// 	fitnessValues[completedIndices[i]] = maxFit;
+// }
+
+
+// let flag = true;
+		// for(let i=0; i<this.members.length; i++){
+			
+		// 	if(this.members[i].crashed == false){
+		// 		flag = false;
+		// 		break;
+		// 	}
+		// }
+		// return flag;
